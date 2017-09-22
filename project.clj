6213afe91ecba35d749bad4da8ceb359e77a311f
Chris J-D <chrisjd-uk@users.noreply.github.com>
2017-09-22 20:50:07 +0100

    Switch to MIT licence.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chrisjd/mapx "0.2.1-SNAPSHOT"
   :description "A simple library for powerful map transformations."
   :url "https://github.com/chrisjd-uk/mapx"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
